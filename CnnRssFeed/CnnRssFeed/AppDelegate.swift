fd48d2887aa98fd05b0639ad893cdc8251ac04cb
Pavel Nadolski <tbigad@gmail.com>
2019-11-26 19:02:25 +0300

    init project

---
+++ CnnRssFeed/CnnRssFeed/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  CnnRssFeed
+//
+//  Created by Pavel N on 11/26/19.
+//  Copyright © 2019 Pavel N. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.       
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        let navigationController = UINavigationController(rootViewController: UITableViewController())
+        navigationController.title = "Title"
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+}
+
