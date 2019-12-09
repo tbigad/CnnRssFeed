e8b7aa54515136edeb5da5f4b49704f31d6d6e00
Pavel Nadolski <tbigad@gmail.com>
2019-12-09 22:51:53 +0300

    add assest

--- CnnRssFeed/CnnRssFeed/Modules/NewsFeed/NewsFeedView.swift
+++ CnnRssFeed/CnnRssFeed/Modules/NewsFeed/NewsFeedView.swift
@@ -9,29 +9,38 @@
 import UIKit
 
 class NewsFeedView: BaseView {
-    lazy var closeButton:UIButton = UIButton()
+    lazy var closeButton:UIButton = UIButton(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
     lazy var tableView:UITableView = UITableView()
+    var buttonBottomAnchor:NSLayoutConstraint!
     
     override func setupLayout() {
         closeButton.setImage(UIImage(named: "close"), for: .normal)
         tableView.autoresizingMask = [.flexibleWidth,.flexibleHeight]
+        closeButton.autoresizingMask = [.flexibleWidth,.flexibleHeight]
         tableView.translatesAutoresizingMaskIntoConstraints = false
+        closeButton.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate(
         [tableView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
         tableView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor),
         tableView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor),
-        tableView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor)])
+        tableView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor),
+        closeButton.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 0),
+        closeButton.heightAnchor.constraint(equalToConstant: 75),
+        closeButton.widthAnchor.constraint(equalToConstant: 75)])
+        
+        
+        buttonBottomAnchor = closeButton.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: 0)
+        buttonBottomAnchor.isActive = true
+        
     }
     
     override func addSubviews() {
         addSubview(tableView)
         addSubview(closeButton)
+        if #available(iOS 13.0, *) {
+            backgroundColor = .systemBackground
+        } else {
+            backgroundColor = .white
+        }
     }
-    
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        let buttonWidth = tableView.bounds.size.width/5
-        closeButton.frame = CGRect(x: 0, y: tableView.bounds.size.height, width: buttonWidth, height: buttonWidth)
-    }
-    
 }
